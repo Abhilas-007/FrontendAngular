a44bba71ae416c445c4b050271690736a525261c
Shubham Prabhat Jha <shubham.jha2@mindtree.com>
2021-03-02 16:42:16 +0530

    second commit

---
+++ src/app/clerk/clerk.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClerkRoutingModule } from './clerk-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ClerkRoutingModule
+  ]
+})
+export class ClerkModule { }
