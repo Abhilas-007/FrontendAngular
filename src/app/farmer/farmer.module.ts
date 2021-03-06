cdb3e31afb3e621c8c42b735ac4217f9360e9e7f
Chaitra Nuthanakalva <chaitra.nuthanakalva@mindtree.com>
2021-03-06 10:24:00 +0530

    added header and footer in farmer dashboard

--- src/app/farmer/farmer.module.ts
+++ src/app/farmer/farmer.module.ts
@@ -7,6 +7,9 @@ import { MspTabComponent } from './msp-tab/msp-tab.component';
 import { TransactionTabComponent } from './transaction-tab/transaction-tab.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [FarmerComponent, MspTabComponent, TransactionTabComponent],
@@ -14,7 +17,12 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     FarmerRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class FarmerModule { }
