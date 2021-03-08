dad7be55504031a0c8edb6f3a3eb165d15e02523
Shubham Prabhat Jha <shubham.jha2@mindtree.com>
2021-03-08 19:06:02 +0530

    Minor bug fix in admin module

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [{ path: '', component: AdminComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), HomePageModule],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AdminRoutingModule { }
