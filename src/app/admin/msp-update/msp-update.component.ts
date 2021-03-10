3d987e95393111b63b90d2c2f69d63dda986c8e1
Nusrath Anjum <nusrath.anjum2@mindtree.com>
2021-03-10 15:56:13 +0530

    msp update validations

--- src/app/admin/msp-update/msp-update.component.ts
+++ src/app/admin/msp-update/msp-update.component.ts
@@ -12,6 +12,7 @@ import { AdminService } from '../admin.service';
 export class MspUpdateComponent implements OnInit {
 
   crop: Crop = new Crop();
+  disable: boolean = true;
 
   constructor(private adminService: AdminService,private router: Router) { }
 
@@ -22,18 +23,29 @@ export class MspUpdateComponent implements OnInit {
     console.log(cname);
     this.adminService.getMSP(localStorage.getItem('userId'),cname).subscribe(data => {
       this.crop = data;
-    }, error => window.alert("Invalid Crop Name")
+      document.getElementById('field').setAttribute('style','display:block');
+      this.disable = false;
+    }, error => {
+      window.alert("Invalid Crop Name");
+      window.location.reload();
+    }
     );
 
   }
 
-  onSubmit(form: NgForm){
+  onSubmit(form: NgForm,cname: string){
+    this.adminService.getMSP(localStorage.getItem('userId'),cname).subscribe(data => {
+      this.crop = data;
+    }, error => {
+      window.alert("Invalid Crop Name");
+      window.location.reload();
+    }
+    );
     this.adminService.updateMSP(form.value).subscribe(data => {
       this.crop = data;
-    });
-
-    window.alert("MSP Updated Successfuly");
-    window.location.reload();
+      });
+      window.alert("MSP Updated Successfuly");
+      window.location.reload();
   }
 
   onCancel(){
