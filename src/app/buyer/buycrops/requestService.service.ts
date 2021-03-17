0493d3eb1a4b3083923d005d6f64472dbf174e9a
Jyothsna Pasunuti <pasunuti.jyothsna@mindtree.com>
2021-03-17 17:52:46 +0530

    update clerk

--- src/app/buyer/buycrops/requestService.service.ts
+++ src/app/buyer/buycrops/requestService.service.ts
@@ -8,7 +8,7 @@ import { BuyerRequest } from 'src/app/shared/models/BuyerRequest';
 })
 export class RequestServiceService {
 
-  private baseUrl="http://localhost:8080/buyer/add-buyerRequest";
+  private baseUrl="https://alok-emandi-dec-20-dev-api.azurewebsites.net/buyer/add-buyerRequest";
   constructor(private httpClient:HttpClient) { }
   saveRequest(buyer:BuyerRequest):Observable<object>{
     return this.httpClient.post(`${this.baseUrl}`, buyer);
