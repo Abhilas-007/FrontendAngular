25ab092187750847c1561fe5c63eb98fffa4c112
Nusrath Anjum <nusrath.anjum2@mindtree.com>
2021-03-24 13:21:29 +0530

    bugs resolving

--- src/app/admin/view-crop/view-crop.component.spec.ts
+++ src/app/admin/view-crop/view-crop.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ViewCropComponent } from './view-crop.component';
 
 describe('ViewCropComponent', () => {
@@ -8,6 +9,10 @@ describe('ViewCropComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule 
+      ],
       declarations: [ ViewCropComponent ]
     })
     .compileComponents();
--- src/app/admin/view-crop/viewcrop.service.spec.ts
+++ src/app/admin/view-crop/viewcrop.service.spec.ts
@@ -2,10 +2,16 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ViewcropService } from './viewcrop.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Viewcrop', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule 
+      ],
       providers: [ViewcropService]
     });
   });
