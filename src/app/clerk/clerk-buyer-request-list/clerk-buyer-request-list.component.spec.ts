9690b71f86bb260e01ebc73ee48d98550c5bdbe4
Shubham Prabhat Jha <shubham.jha2@mindtree.com>
2021-03-24 13:07:19 +0530

    changes in clerk module spec.ts made

--- src/app/clerk/clerk-buyer-request-list/clerk-buyer-request-list.component.spec.ts
+++ src/app/clerk/clerk-buyer-request-list/clerk-buyer-request-list.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ClerkBuyerRequestListComponent } from './clerk-buyer-request-list.component';
 
@@ -8,6 +10,10 @@ describe('ClerkBuyerRequestListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule 
+      ],
       declarations: [ ClerkBuyerRequestListComponent ]
     })
     .compileComponents();
--- src/app/clerk/clerk.component.spec.ts
+++ src/app/clerk/clerk.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ClerkComponent } from './clerk.component';
 
@@ -8,6 +10,10 @@ describe('ClerkComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule 
+      ],
       declarations: [ ClerkComponent ]
     })
     .compileComponents();
