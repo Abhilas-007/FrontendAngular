079bce9c0a5064f6f32f7acd681ef50e33329ee1
Shubham Prabhat Jha <shubham.jha2@mindtree.com>
2021-03-24 12:43:09 +0530

    minor bug fix in logout service

--- src/app/core/logout.service.spec.ts
+++ src/app/core/logout.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LogoutService } from './logout.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LogoutService', () => {
   let service: LogoutService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule 
+      ],
+    });
     service = TestBed.inject(LogoutService);
   });
 
