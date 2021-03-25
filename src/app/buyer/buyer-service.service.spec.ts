b0c959857f790dd9e87d058f9f7df6ec12b3ca53
Nusrath Anjum <nusrath.anjum2@mindtree.com>
2021-03-25 21:45:30 +0530

    creating unit test

--- src/app/buyer/buyer-service.service.spec.ts
+++ src/app/buyer/buyer-service.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -6,6 +6,7 @@ import { BuyerServiceService } from './buyer-service.service';
 
 describe('BuyerServiceService', () => {
   let service: BuyerServiceService;
+  let httpTestCtrl: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,9 +16,17 @@ describe('BuyerServiceService', () => {
       ]
     });
     service = TestBed.inject(BuyerServiceService);
+    httpTestCtrl = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should test HttpClient get', () => {
+    const testGet = 1;
+    service.getBuyer(testGet).subscribe(data => {
+      expect(data).toBe({"buyerId":1,"buyerName":"nusrath","password":"anjum15","phoneNumber":"7259258255"})
+    });
   });
 });
