589eedda17a0152887485401c941bd91a2e37fa4
Jyothsna Pasunuti <pasunuti.jyothsna@mindtree.com>
2021-03-26 20:29:12 +0530

    added farmer test cases

--- src/app/farmer/farmer-service.service.spec.ts
+++ src/app/farmer/farmer-service.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -6,7 +6,7 @@ import { FarmerServiceService } from './farmer-service.service';
 
 describe('FarmerServiceService', () => {
   let service: FarmerServiceService;
-
+  let httpTestCtrl: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -20,4 +20,22 @@ describe('FarmerServiceService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should test HttpClient get', () => {
+    const testGet = 1;
+    service.getFarmer(testGet).subscribe(data => {
+      expect(data).toBe({"farmerName":"robin","bankName":"SBI", "accountNumber":"55159521645","ifsc":"SBIN0001561","aadharNumber":"259891957247",
+    "password":"v","mobileNumber":"8546121045","securityQuestion":"cityName","answer":"pune","farmerId":1});
+    });
+  });
+
+
+  
+  it('should test HttpClient get', () => {
+    const testGet = 1;
+    service.getTransactions().subscribe(data => {
+      expect(data).toBe({"farmerName":"robin","bankName":"SBI", "accountNumber":"55159521645","ifsc":"SBIN0001561","aadharNumber":"259891957247",
+    "password":"v","mobileNumber":"8546121045","securityQuestion":"cityName","answer":"pune","farmerId":1});
+    });
+  }); 
+
 });
