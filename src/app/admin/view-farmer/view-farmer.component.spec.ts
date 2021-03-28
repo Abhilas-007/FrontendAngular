2705d6a7ec624cb2888dc5540b3249f521c7f2bc
Chaitra Nuthanakalva <chaitra.nuthanakalva@mindtree.com>
2021-03-28 12:08:33 +0530

    added a custom test case to get all farmers

--- src/app/admin/view-farmer/view-farmer.component.spec.ts
+++ src/app/admin/view-farmer/view-farmer.component.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ViewFarmerComponent } from './view-farmer.component';
@@ -40,4 +40,9 @@ describe('ViewFarmerComponent', () => {
   it('should create', () => {
     expect(component.onMandiPincodeSearch()).toBe();
   });
+
+  it(`should get the farmers`, waitForAsync( () => {
+    expect(component.getFarmers.length).toBeDefined();
+  }
+  ));
 });
